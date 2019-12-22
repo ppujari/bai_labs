0746372668381d956da1056be66848702fe7b02f
Amiya Panigrahi <eee.amiya@gmail.com>
2019-12-22 10:39:37 +0530

    Added the graph

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from pymongo import MongoClient
 from pprint import pprint
 
 import configparser
 import database
+import plot
 
 
 from datetime import datetime
@@ -39,10 +40,31 @@ database.connect_mongo(host,'traffic_data','alpha')
 
 app = Flask(__name__)
 
-
+weekdays=[]
+count=[]
 @app.route('/')
 def index():
+#  database.get_from_mongo()
  return render_template('home.html')
+
+@app.route('/plot.png')
+def plot1():
+  weekdays, counts = database.get_from_mongo(90)
+  return plot.plot_png(weekdays, counts)
+
+@app.route('/chart',methods=['GET', 'POST'])
+def chart():
+  if request.method == 'POST':
+    value= request.form['ABC']
+    # print(value)
+    value=int(value)
+    data = database.get_from_mongo(value)
+  else:
+    print("amiya")
+    data = database.get_from_mongo(30)
+    # print(type(data))
+  return render_template("chart.html",data=data )
+
 
 @app.route('/alpha')
 def index_alpha():
@@ -59,6 +81,7 @@ def index_alpha():
       database.write_to_mongo(mongo_docs)
       time.sleep(2)
   return render_template('alpha.html')
+
 
 if __name__ == '__main__':
   app.run(debug=True)
--- database.py
+++ database.py
@@ -1,6 +1,11 @@
 from pymongo import MongoClient
+from datetime import date, timedelta
+import json
+from bunch import bunchify
+from datetime import datetime
 
 col = ""
+days=[]
 def connect_mongo(ip, collection='traffic_data', database='alpha'):
     global col
     connect=1
@@ -17,6 +22,73 @@ def connect_mongo(ip, collection='traffic_data', database='alpha'):
     if connect==1:
         print("connected")
     return connect
+
+def get_from_mongo(value):
+    global col
+    global days
+    # print(col)
+    pipeline=[{'$group': { '_id': '$time_stamp', 'count':{'$sum':1}}},{'$sort':{'_id': 1}}]
+    # values= list(col.aggregate(pipeline))
+    values= list(col.aggregate(pipeline))
+    # count = col.count_documents({})
+    
+    # print(type(values))
+    # print(values)
+
+    
+    now = datetime.now() # current date and time
+    # weekday= now.weekday()
+    # weekdays=[]
+    # weekdays.append((now-timedelta(weekday)).strftime("%Y/%m/%d"))
+    date_time = now.strftime("%Y/%m/%d")
+    print("date and time:",date_time)
+    
+    # print(weekday)
+
+
+    # for x in range(7):
+    #     if(x<weekday):
+    #         weekdays.append((now-timedelta(weekday-x)).strftime("%Y-%m-%d"))
+    #     elif(x==weekday):
+    #          weekdays.append((now-timedelta(0)).strftime("%Y-%m-%d"))
+    #     else:
+    #          weekdays.append((now+timedelta(x-weekday)).strftime("%Y-%m-%d"))
+
+    # for x in range(7):
+    #     weekdays.append((now-timedelta(6-x)).strftime("%Y-%m-%d"))
+    # print(weekdays)  
+    # print(len(values))
+    counts=[]
+    filter(value)
+    # print('amiya')
+    for x in range(len(values)):
+        beb=values[x]
+        ami=beb['_id']
+        # print(ami)
+        for y in days:
+            if(ami==y):
+                counts.append(beb['count'])
+    # print(weekdays)
+    # print(counts)
+
+    data={
+        "x": days,
+        "y": counts
+    }
+    print("data")
+
+    return data
+
+
+def filter(fil):
+    global days
+    days=[]
+    now = datetime.now()
+    
+    for x in range(fil):
+        days.append((now-timedelta(fil-x-1)).strftime("%Y-%m-%d"))
+        
+    print(days)
 
 def write_to_mongo(mongo_docs):
 
